47527f948d8350a1260ccbb3dfa4be42efbae551
Tristan Bagot <tristan.bagot@gmail.com>
2019-03-07 15:01:47 +0100

    fetch

--- kirby-cloudinary.php
+++ kirby-cloudinary.php
@@ -3,4 +3,3 @@
 require 'vendor/autoload.php';
 
 require_once __DIR__ . DS . 'lib' . DS . 'init.php';
-require_once __DIR__ . DS . 'helpers.php';
--- lib/fallback.php
+++ lib/fallback.php
@@ -1,24 +1,18 @@
 <?php
 
-file::$methods['cloudinary_url'] = function($file, $options = array('')) {
-  $file_url = '';
+file::$methods['cl_crop'] = function($file, $width, $height, $params = []) {
+	$params = array_merge(
+		[ "width" => $width, "height" => $height, "crop" => true, "gravity" => "auto"],
+		$params,
+		c::get('kirby.cloudinary.default_params')
+	);
+	return $file->thumb($params)->url();
+};
 
-  switch ($file->type()) {
-    case 'image':
-      $main_options = array(
-        'fetch_format'=>'auto',
-        'type'=>'fetch',
-        'quality' => 'auto',
-        'format' => 'auto',
-        'crop' => 'limit',
-      );
-      $options = array_merge($options, $main_options);
-      $file_url = $file->width(3000)->url();
-      break;
-
-    default:
-      # code...
-      break;
-  }
-  return $file_url;
+file::$methods['cl_thumb'] = function($file, $params = []) {
+	$params = array_merge(
+		$params,
+		c::get('kirby.cloudinary.default_params')
+	);
+	return $file->thumb($params)->url();
 };
